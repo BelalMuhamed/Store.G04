f1d7883f4170ace27e35ca643a9812c0c1f4ae7e
BelalBasal <basdibrahim@gmail.com>
2024-10-07 20:24:57 +0300

    Add project files.

---
+++ Store.G04.Core/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.Core.Entities
+{
+    internal class Product:BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string Descreption { get; set; }
+        public string PictureUrl { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+    }
+}
