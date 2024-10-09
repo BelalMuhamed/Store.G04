20d4e303edc926f61463667de4af8c0411c8847b
BelalBasal <basdibrahim@gmail.com>
2024-10-09 19:27:35 +0300

    ResolvePictureURL

--- Store.G04.APIs/Program.cs
+++ Store.G04.APIs/Program.cs
@@ -27,7 +27,7 @@ namespace Store.G04.APIs
             builder.Services.AddDbContext<StoreDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddScoped<IProductService, ProductService>();
             builder.Services.AddScoped<IUnitOfWork,UnitOfWork>();
-            builder.Services.AddAutoMapper(m => m.AddProfile(new ProductProfile()));
+            builder.Services.AddAutoMapper(m => m.AddProfile(new ProductProfile(builder.Configuration)));
             var app = builder.Build();
           using var Scope = app.Services.CreateScope();
             var Services = Scope.ServiceProvider;
@@ -55,7 +55,7 @@ namespace Store.G04.APIs
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
+            app.UseStaticFiles();
 
             app.MapControllers();
 
